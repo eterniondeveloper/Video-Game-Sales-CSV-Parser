36ceb1e1ea5ed3cf8768eb38463363a1c31208d3
konstantinos <konstantinos_raptis@hotmail.com>
2017-04-26 21:19:27 +0300

    Drag n Drop + CSV Parsing implemented successfully

---
+++ src/main/java/vg/sales/App.java
@@ -0,0 +1,89 @@
+package vg.sales;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Preloader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import vg.sales.gui.AbstractJavaFxApplicationSupport;
+import vg.sales.model.VGSale;
+import vg.sales.reader.CSVReader;
+
+@Lazy
+@SpringBootApplication
+public class App extends AbstractJavaFxApplicationSupport {
+
+    /**
+     * Note that this is configured in application.properties
+     */
+    @Value("${app.ui.title:Example App}")
+    private String windowTitle;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        notifyPreloader(new Preloader.StateChangeNotification(Preloader.StateChangeNotification.Type.BEFORE_START));
+
+        Group root = new Group();
+        Scene scene = new Scene(root, 551, 400);
+
+        scene.setOnDragOver((DragEvent event) -> {
+
+            Dragboard db = event.getDragboard();
+
+            if (db.hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY);
+            } else {
+                event.consume();
+            }
+        });
+
+        // Dropping over surface
+        scene.setOnDragDropped((DragEvent event) -> {
+
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+
+            if (db.hasFiles()) {
+                
+                success = true;
+                String filePath;
+                
+                for (File file : db.getFiles()) {
+                    filePath = file.getAbsolutePath();
+
+                    try {
+                        List<VGSale> vgSaleList = new CSVReader().read(filePath, true);
+                        vgSaleList.forEach(System.out::println);
+                    } catch (IOException ex) {
+                        Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                }
+            }
+
+            event.setDropCompleted(success);
+            event.consume();
+        });
+
+        stage.setTitle(windowTitle);
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launchApp(App.class, args);
+    }
+
+}
--- src/main/java/vg/sales/reader/CSVReader.java
+++ src/main/java/vg/sales/reader/CSVReader.java
@@ -23,37 +23,39 @@ public class CSVReader {
         BufferedReader br = new BufferedReader(new FileReader(file));
         VGSale sale;
         String[] data;
-        
+
         while ((line = br.readLine()) != null) {
-            
+
             if (isFirstLineHeaders) {
                 isFirstLineHeaders = false;
                 continue;
             }
-            
+
             // use comma as separator
             data = line.split(cvsSplitBy);
             sale = new VGSale();
-            
+
             // extract sales data
-            sale.setRank(Long.parseLong(data[0]));
-            sale.setName(data[1]);
-            sale.setPlatform(data[2]);
-            sale.setYear(Integer.parseInt(data[3]));
-            sale.setGenre(data[4]);
-            sale.setPublisher(data[5]);
-            sale.setNaSales(Double.parseDouble(data[6]));
-            sale.setEuSales(Double.parseDouble(data[7]));
-            sale.setJpSales(Double.parseDouble(data[8]));
-            sale.setOtherSales(Double.parseDouble(data[9]));
-            sale.setGlobalSales(Double.parseDouble(data[10]));
-            
+            try {
+                sale.setRank(Long.parseLong(data[0]));
+                sale.setName(data[1]);
+                sale.setPlatform(data[2]);
+                sale.setYear(Integer.parseInt(data[3]));
+                sale.setGenre(data[4]);
+                sale.setPublisher(data[5]);
+                sale.setNaSales(Double.parseDouble(data[6]));
+                sale.setEuSales(Double.parseDouble(data[7]));
+                sale.setJpSales(Double.parseDouble(data[8]));
+                sale.setOtherSales(Double.parseDouble(data[9]));
+                sale.setGlobalSales(Double.parseDouble(data[10]));
+            } catch (NumberFormatException ex) {
+                throw new IOException(ex.getMessage());
+            }
+
             vgSaleList.add(sale);
-            
-            // System.out.println(sale);
         }
 
         return vgSaleList;
     }
-    
+
 }
